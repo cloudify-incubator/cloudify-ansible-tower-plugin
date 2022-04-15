0d92b7929863d9e1f3569a3995d3c03bc54ec067
Jakub Cierlik <jakub.cierlik@amartus.com>
2022-04-15 11:19:44 +0200

    version upgrade

--- setup.py
+++ setup.py
@@ -14,11 +14,30 @@
 #    * limitations under the License.
 '''Ansible Tower / AWX plugin for Cloudify package config'''
 
+import os
 from setuptools import setup
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_file='plugin.yaml'):
+    lines = read(rel_file)
+    for line in lines.splitlines():
+        if 'package_version' in line:
+            split_line = line.split(':')
+            line_no_space = split_line[-1].replace(' ', '')
+            line_no_quotes = line_no_space.replace('\'', '')
+            return line_no_quotes.strip('\n')
+    raise RuntimeError('Unable to find version string.')
+
 
 setup(
     name='cloudify-ansible-tower-plugin',
-    version='1.0.0',
+    version=get_version(),
     license='LICENSE',
     packages=[
         'cloudify_ansible_tower',
